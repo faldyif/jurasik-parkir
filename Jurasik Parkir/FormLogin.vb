906f0f5a00f9b5ad74d95649bfaaba1cd30084e5
faldyif <faldy.if@gmail.com>
2017-04-20 08:29:51 +0700

    Implement login

--- Jurasik Parkir/FormLogin.vb
+++ Jurasik Parkir/FormLogin.vb
@@ -1,5 +1,55 @@
-﻿Public Class FormLogin
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormLogin
+    Dim koneksi As New MySqlConnection
+
+    Private Sub openKoneksi()
+        koneksi.ConnectionString =
+        "server=156.67.208.181;user=u5239788_jurasik;password={s]X=Terp}cc;database=u5239788_jurasikparkir"
+        koneksi.Open()
+    End Sub
+
+    Private Sub closeKoneksi()
+        koneksi.Close()
+    End Sub
+
+    Private Sub eksekusiLogin(ByVal username As String, ByVal password As String)
+        openKoneksi()
+
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM petugas_parkir WHERE username = '" & username & "' AND password = '" & password & "'", koneksi)
+        Dim table As New DataTable
+        adapter.Fill(table)
+        If (table.Rows.Count.Equals(1)) Then
+            If (table.Rows(0)(3).Equals(1)) Then
+                Dim form = New FormKendaraanMasuk()
+                form.Show()
+                Me.Close()
+            ElseIf (table.Rows(0)(3).Equals(2)) Then
+                Dim form = New FormKendaraanKeluar()
+                form.Show()
+                Me.Close()
+            ElseIf (table.Rows(0)(3).Equals(3)) Then
+                Dim form = New FormListKendaraan()
+                form.Show()
+                Me.Close()
+            End If
+        Else
+            MessageBox.Show("Username atau password anda salah!")
+        End If
+
+        koneksi.Close()
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String
+        Dim password As String
+        username = TextBox1.Text
+        password = TextBox2.Text
+
+        eksekusiLogin(username, password)
     End Sub
 End Class
